d285de6ee6500f1fb891094be309e9d2ccc13b23
steelerx155 <steelerx155@gmail.com>
2020-05-13 18:16:21 -0400

    Done.

--- .history/index_20200513162750.js
+++ .history/index_20200513162750.js
@@ -11,12 +11,6 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = (input) => {
-  return tutorials.map( line => {
-    let tokens = line.split(' ')
-    let capitalizedTokens =
-      tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1) )
-    let response = capitalizedTokens.join(' ')
-    return response
-  })
+const titleCased = () => {
+  return tutorials
 }
