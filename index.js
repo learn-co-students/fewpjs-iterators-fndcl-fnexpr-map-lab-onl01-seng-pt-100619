33a0cee6005f7cab8ec9f628d19ccf8e5af17d2b
JTSwisher <jefftswisher@gmail.com>
2020-05-16 10:15:28 -0700

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function (sentance) {
+    let words = sentance.split(" ");
+    let capitalizedWords = words.map( word => 
+      word.charAt(0).toUpperCase() + word.slice(1) )
+    let result = capitalizedWords.join(' ')
+    return result
+  })
 }
+
+
+
